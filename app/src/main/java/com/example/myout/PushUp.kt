f48b2b1b268738e5830962a5e34e1c5b284c30eb
AssimR <43984858+AssimR@users.noreply.github.com>
2021-07-30 18:31:35 +0200

    pause

--- app/src/main/java/com/example/myout/PushUp.kt
+++ app/src/main/java/com/example/myout/PushUp.kt
@@ -1,19 +1,24 @@
 package com.example.myout
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 class PushUp : AppCompatActivity() {
 
 
-    val SetArray = arrayOf<Int>(15, 23, 51, 22, 13)
 
-    var noOfSets = 1;
+    private val SetArray = arrayOf<Int>(15, 23, 51, 22, 13)
+
+    var noOfSets = 3;
 
     var started1 = false;
     var started2 = false;
@@ -24,15 +29,46 @@ class PushUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_push_up)
-
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         //bottomNavigation.setSelectedItemId(R.id.home)
         bottomNavigation.setOnNavigationItemSelectedListener(navigationBar)
 
 
         var btn = findViewById<Button>(R.id.PushUpButton)
+
         btn.text = SetArray[0].toString()
 
+
+
+
+    }
+
+    private fun pause()
+    {
+        var btn = findViewById<Button>(R.id.PushUpButton)
+        var txt = findViewById<TextView>(R.id.text)
+
+        object : CountDownTimer(59000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+
+                txt.text = "Pause"
+
+
+                val f: NumberFormat = DecimalFormat("00")
+                val sec = millisUntilFinished / 1000 % 60
+                btn.text = f.format(sec)
+                btn.setTextColor(Color.parseColor("#a7fab0"))
+                btn.isEnabled = false
+            }
+
+            // When the task is over it will print 00:00:00 there
+            override fun onFinish() {
+                btn.text = "Click to start"
+                btn.isEnabled = true
+                btn.setTextColor(Color.parseColor("White"))
+                txt.text = "Push Up"
+            }
+        }.start()
     }
 
     private val navigationBar = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -63,8 +99,12 @@ class PushUp : AppCompatActivity() {
 
     }
 
+
+
+
     fun onTap(view: View) {
         val btn = findViewById<Button>(R.id.PushUpButton)
+
 
         if (SetArray[0].toInt() != 1 )
         {
@@ -80,8 +120,18 @@ class PushUp : AppCompatActivity() {
                 btn.isEnabled = false;
             }
             else {
-                btn.text = SetArray[1].toString()
-                SetArray[1] --
+                if (!started1)
+                {
+                    pause()
+                    started1 = true
+                }
+                else
+                {
+                    btn.text = SetArray[1].toString()
+                    SetArray[1] --
+                }
+
+
             }
 
 
@@ -93,8 +143,17 @@ class PushUp : AppCompatActivity() {
                 btn.isEnabled = false;
             }
             else {
-                btn.text = SetArray[2].toString()
-                SetArray[2] --
+                if (!started2)
+                {
+                    pause()
+                    started2 = true
+                }
+                else
+                {
+                    btn.text = SetArray[2].toString()
+                    SetArray[2] --
+                }
+
             }
 
 
@@ -106,8 +165,18 @@ class PushUp : AppCompatActivity() {
                 btn.isEnabled = false;
             }
             else {
-                btn.text = SetArray[3].toString()
-                SetArray[3] --
+                if (!started3)
+                {
+                    pause()
+                    started3 = true
+                }
+                else
+                {
+                    btn.text = SetArray[3].toString()
+                    SetArray[3] --
+                }
+
+
             }
 
         }
@@ -119,8 +188,19 @@ class PushUp : AppCompatActivity() {
                 btn.isEnabled = false;
             }
             else {
-                btn.text = SetArray[4].toString()
-                SetArray[4] --
+
+                if (!started4)
+                {
+                    pause()
+                    started4 = true
+                }
+                else
+                {
+                    btn.text = SetArray[4].toString()
+                    SetArray[4] --
+                }
+
+
             }
 
         }
