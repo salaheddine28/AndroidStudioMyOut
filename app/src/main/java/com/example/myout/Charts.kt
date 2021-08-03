a028ddcc6a77ea48390cf2ffcc473da9ef53e410
AssimR <43984858+AssimR@users.noreply.github.com>
2021-08-03 14:25:42 +0200

    chart

--- app/src/main/java/com/example/myout/Charts.kt
+++ app/src/main/java/com/example/myout/Charts.kt
@@ -3,6 +3,11 @@ package com.example.myout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.anychart.AnyChart
+import com.anychart.AnyChartView
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Charts : AppCompatActivity() {
@@ -13,6 +18,20 @@ class Charts : AppCompatActivity() {
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigation.setSelectedItemId(R.id.ic_bar)
         bottomNavigation.setOnNavigationItemSelectedListener(navigationBar)
+
+
+        val pie = AnyChart.pie()
+
+        val data: MutableList<DataEntry> = ArrayList()
+        data.add(ValueDataEntry("John", 10000))
+        data.add(ValueDataEntry("Jake", 12000))
+        data.add(ValueDataEntry("Peter", 18000))
+
+        pie.data(data)
+
+        val anyChartView = findViewById<View>(R.id.any_chart_view) as AnyChartView
+        anyChartView.setChart(pie)
+
     }
 
     private val navigationBar = BottomNavigationView.OnNavigationItemSelectedListener { item ->
