6170aaab0aff1e898da2da57dda60f9aab83510a
hamzamaazouz <43775348+hamzamaazouz@users.noreply.github.com>
2021-08-09 22:10:37 +0200

    Delete Users.kt

--- app/src/main/java/com/example/myout/Users.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myout
-
-import java.net.PasswordAuthentication
-
-data class Users(val firstName : String? = null, val lastName : String? = null,
-                 val email : String? = null, val height : String? = null, val weight : String? = null, val birthday : String? = null){
-
-}
