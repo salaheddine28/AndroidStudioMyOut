00a14f99c451904760b825f7ffedda532b02bff6
hamzamaazouz <43775348+hamzamaazouz@users.noreply.github.com>
2021-08-14 23:31:31 +0200

    Profile

--- app/src/main/java/com/MyOut/myout/Profile.kt
+++ app/src/main/java/com/MyOut/myout/Profile.kt
@@ -34,7 +34,7 @@ class Profile : AppCompatActivity() {
 
 
         btn_Save.setOnClickListener {
-
+            UpdateProfile()
         }
 
         btn_Settings.setOnClickListener {
@@ -48,6 +48,23 @@ class Profile : AppCompatActivity() {
         bottomNavigation.setOnNavigationItemSelectedListener(navigationBar)
 
 
+    }
+    private fun UpdateProfile(){
+
+        val db = FirebaseFirestore.getInstance()
+
+        val firstName = firstname.text.toString()
+        val lastName = lastname.text.toString()
+        val ht = height.text.toString()
+        val wt = weight.text.toString()
+
+        val user = hashMapOf(
+            "Firstname" to firstName,
+            "Lastname" to lastName,
+            "height" to ht,
+            "weight" to wt
+        )
+        db.collection("users").document(auth.currentUser!!.uid).set(user)
     }
 
     private fun loadProfile() {
@@ -66,10 +83,10 @@ class Profile : AppCompatActivity() {
                 if (document != null){
                     Log.d("Exist", "DocumentSnapchot data: ${document.data}")
 
-                    firstname.text = document.getString("Firstname")
-                    lastname.text = document.getString("Lastname")
-                    height.text = document.getString("height")
-                    weight.text = document.getString("weight")
+                    firstname.hint = document.getString("Firstname")
+                    lastname.hint = document.getString("Lastname")
+                    height.hint = document.getString("height")
+                    weight.hint = document.getString("weight")
                 }else {
                     Log.d("noexist", "No such document")
                 }
