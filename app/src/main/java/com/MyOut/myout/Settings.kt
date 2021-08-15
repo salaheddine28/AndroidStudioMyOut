4719df4458c1cbe9f8e47b4226d3104945b1ea27
salaheddine28 <44190669+salaheddine28@users.noreply.github.com>
2021-08-15 17:44:35 +0200

    disconnect

--- app/src/main/java/com/MyOut/myout/Settings.kt
+++ app/src/main/java/com/MyOut/myout/Settings.kt
@@ -1,18 +1,37 @@
 package com.MyOut.myout
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class Settings : AppCompatActivity() {
+
+    lateinit var preferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigation.setOnNavigationItemSelectedListener(navigationBar)
-        bottomNavigation.setSelectedItemId(R.id.ic_profile)
+        //bottomNavigation.setSelectedItemId(R.id.ic_profile)
+
+        preferences = getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+
+        
+        btn_Disconnect.setOnClickListener{
+            val editor: SharedPreferences.Editor = preferences.edit()
+            editor.clear()
+            editor.apply()
+
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+
     }
 
     private val navigationBar = BottomNavigationView.OnNavigationItemSelectedListener { item ->
